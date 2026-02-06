d77a09241d80ccd9e13f2c6b8f99f98b29394b7c
VladimirTalpa <157706039+VladimirTalpa@users.noreply.github.com>
2026-02-06 22:06:32 +0200

    Delete src/core directory

--- src/core/prisma.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === "production" ? ["error"] : ["error", "warn"],
-});
-
-module.exports = { prisma };
--- src/core/redis.js
+++
@@ -1,8 +0,0 @@
-const IORedis = require("ioredis");
-
-const redis = new IORedis(process.env.REDIS_URL, {
-  maxRetriesPerRequest: null,
-  enableReadyCheck: true,
-});
-
-module.exports = { redis };
