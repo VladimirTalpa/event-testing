7f34055409a7e3048641a4dc109f635f6de7eb9c
VladimirTalpa <157706039+VladimirTalpa@users.noreply.github.com>
2026-02-07 08:23:54 +0200

    Add maps for channel and user state management

--- src/state.js
+++ src/state.js
@@ -1,1 +1,7 @@
 
+const bossByChannel = new Map();
+const hollowByChannel = new Map();
+const clashByChannel = new Map();
+const lastClashByUser = new Map();
+
+module.exports = { bossByChannel, hollowByChannel, clashByChannel, lastClashByUser };
