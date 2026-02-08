f948c23920eed78cf3f268a839ebeb78527ed1b0
VladimirTalpa <157706039+VladimirTalpa@users.noreply.github.com>
2026-02-08 17:24:50 +0200

    Update media.js

--- src/data/media.js
+++ src/data/media.js
@@ -1,57 +1,134 @@
 // src/data/media.js
 
+/* ===================== BLEACH MEDIA ===================== */
+const VASTO_SPAWN_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469842057219674194/Your_paragraph_text_13.gif?ex=69892096&is=6987cf16&hm=c31783bb0a9a57c197a3faf8d9314fb2a1d4621d424c8961bfcb2c0f0c753ef3&=";
 
-/* ========== GRIMMJOW ========== */
+const VASTO_R1 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469842005583462514/Your_paragraph_text_16.gif?ex=6989208a&is=6987cf0a&hm=f9a4c88976d44e3581b82d55c01fdefb03f1c7401697c8a663cf6aeeff68e8c3&=";
 
-const GRIMMJOW_SPAWN =
-"https://media.discordapp.net/attachments/1405973335979851877/1469843123160088636/Your_paragraph_text_20.gif";
+const VASTO_R2 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469842043227341068/Your_paragraph_text_14.gif?ex=69892093&is=6987cf13&hm=117ea0c95417384a7b790f746a774d0778b9348257fd8ee7422ed8c4e908dd9a&=";
+
+const VASTO_R3 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469842024172884138/Your_paragraph_text_15.gif?ex=6989208e&is=6987cf0e&hm=ba70c2e8435df2b8aefb26205c5c0fc23386895da4837e5fcae10eb8fdd03d19&=";
+
+const VASTO_R4 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469842068024066068/Your_paragraph_text_12.gif?ex=69892099&is=6987cf19&hm=e1080c7bddf29f2e6edc23f9a083189bde2d417ea9ffb9d81e4b5dcd218227cc&=";
+
+const VASTO_R5 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469841986705166347/Your_paragraph_text_18.gif?ex=69892085&is=6987cf05&hm=3b1a1520ace36d0ab11d4a443bed1f1321488192657b464da15ffb11d4f72700&=";
+
+const VASTO_VICTORY_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469841996616040511/Your_paragraph_text_17.gif?ex=69892088&is=6987cf08&hm=c5adfce5d6fff70c659a87a43d9b1be1b56fdbd52031de45f6c15962306cf37f&=";
+
+const VASTO_DEFEAT_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469842262208020632/Your_paragraph_text_19.gif?ex=698920c7&is=6987cf47&hm=243b0dea5d8bec6a78a4efc223fa07e8e3656c4c301ca7521395bc935ef73b7b&=";
+
+// Ulquiorra gifs
+const ULQ_SPAWN_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843198812618782/Your_paragraph_text_25.gif?ex=698921a6&is=6987d026&hm=8ab0b38b1fafd210a7cbf589f54b37ce4c4e7117e5141a63d6d150e32f71096c&=";
+
+const ULQ_R1 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843217058103556/Your_paragraph_text_26.gif?ex=698921ab&is=6987d02b&hm=4499f79869465416007ef21580b08fdd8c6a8f597521ec484e22c023d3867586&=";
+
+const ULQ_R2 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843235986866196/Your_paragraph_text_27.gif?ex=698921af&is=6987d02f&hm=d73e433123104264fb7797e32267d4af89dc7887fb2efdea42a41e578fc85bf4&=";
+
+const ULQ_R3 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843247999353004/Your_paragraph_text_28.gif?ex=698921b2&is=6987d032&hm=03afda58f47e27975d3b6f5ee7a4af654e3bcc9ff89c8fc7488f3e905509dcbf&=";
+
+const ULQ_R4 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843261139980308/Your_paragraph_text_29.gif?ex=698921b5&is=6987d035&hm=63d6429d4d618c3682ef4665c3b494200ccb031d4f450c38539ee8cde319a1ac&=";
+
+const ULQ_R5 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843274737914123/Your_paragraph_text_30.gif?ex=698921b9&is=6987d039&hm=6274a0db6b1866c2d134fd4b9f200b68e968e97fd2ceb9ca7312c7a8cae804af&=";
+
+const ULQ_R6 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843288277127219/Your_paragraph_text_31.gif?ex=698921bc&is=6987d03c&hm=88e3096454d50f1268761b18320c8d12a23d80cc49ff7c93240e3d7f553e4d6e&=";
+
+const ULQ_VICTORY_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843303930527929/Your_paragraph_text_32.gif?ex=698921c0&is=6987d040&hm=2ad405fd31cd5be31faebe491f651ff5a1bb88a9816eebf0b6aa823808592df8&=";
+
+const ULQ_DEFEAT_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843317087797279/Your_paragraph_text_33.gif?ex=698921c3&is=6987d043&hm=a9a78cb6e341b7d27c4d94b4f1c29c248811d77b206ad4ea7b6f7571fceabd2f&=";
+
+/* ===================== GRIMMJOW (NEW) ===================== */
+const GRIMMJOW_SPAWN_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843123160088636/Your_paragraph_text_20.gif?ex=69892194&is=6987d014&hm=f4f9a53a32821a59c255ab38ca2785aee18654acfb13fad1220324fc93b31431&=";
 
 const GRIMMJOW_R1 =
-"https://media.discordapp.net/attachments/1405973335979851877/1469843137181651024/Your_paragraph_text_21.gif";
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843137181651024/Your_paragraph_text_21.gif?ex=69892198&is=6987d018&hm=4852cd95f921b0d65021bb4569e695eca88078b0b75c31072e307163190e702f&=";
 
 const GRIMMJOW_R2 =
-"https://media.discordapp.net/attachments/1405973335979851877/1469843163945504899/Your_paragraph_text_23.gif";
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843163945504899/Your_paragraph_text_23.gif?ex=6989219e&is=6987d01e&hm=2ac5fdbc70879714bf44c2c107d643cb61b67f79b7e27dbea0fb2a30c7feb861&=";
 
-const GRIMMJOW_WIN =
-"https://media.discordapp.net/attachments/1405973335979851877/1469843182920532062/Your_paragraph_text_24.gif";
+const GRIMMJOW_VICTORY_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843182920532062/Your_paragraph_text_24.gif?ex=698921a3&is=6987d023&hm=7156f4b3cddeb4dc24e79afd243b5967c6ea239b53c1fdb75e6a3ca271546ece&=";
 
-const GRIMMJOW_LOSE =
-"https://media.discordapp.net/attachments/1405973335979851877/1469843151152746668/Your_paragraph_text_22.gif";
+const GRIMMJOW_DEFEAT_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1469843151152746668/Your_paragraph_text_22.gif?ex=6989219b&is=6987d01b&hm=0482ac0a77dc9e0d047b55951c7708d71badbda4d9cc3b6f906d3e445e780dcc&=";
 
+/* ===================== BLEACH MOB (Hollow) ===================== */
+const HOLLOW_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1467508068303638540/Your_paragraph_text_10.gif?ex=6980a2e4&is=697f5164&hm=451cc0ec6edd18799593cf138549ddb86934217f6bee1e6364814d23153ead78&=";
 
-/* ========== JJK SPECIAL ========== */
+/* ===================== JJK BOSS (NEW MEDIA) ===================== */
+const JJK_BOSS_SPAWN_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1470019091216597002/Your_paragraph_text_39.gif?ex=6989c576&is=698873f6&hm=202a603d2f90e20a5cc05442be5725288cbee9f3148115b34e1e38a502b92c8b&=";
 
-const JJK_SPAWN =
-"https://media.discordapp.net/attachments/1405973335979851877/1470019091216597002/Your_paragraph_text_39.gif";
+const JJK_BOSS_R1 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1470019102163730637/Your_paragraph_text_38.gif?ex=6989c579&is=698873f9&hm=15c50afadae2c1ae772b207479ee483d014be35850f95ecc41172fb8d70ab9ea&=";
 
-const JJK_R1 =
-"https://media.discordapp.net/attachments/1405973335979851877/1470019102163730637/Your_paragraph_text_38.gif";
+const JJK_BOSS_R2 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1470019117309235418/Your_paragraph_text_37.gif?ex=6989c57d&is=698873fd&hm=d844521a54938743ea9dddb3787cc0cc9e13405ee0bd053012a234caaeb25115&=";
 
-const JJK_R2 =
-"https://media.discordapp.net/attachments/1405973335979851877/1470019117309235418/Your_paragraph_text_37.gif";
+const JJK_BOSS_R3 =
+  "https://media.discordapp.net/attachments/1405973335979851877/1470019129867239526/Your_paragraph_text_36.gif?ex=6989c580&is=69887400&hm=7793353f116000d95fb393f34a41586e9fe7300005d00cd8862e34226041c766&=";
 
-const JJK_R3 =
-"https://media.discordapp.net/attachments/1405973335979851877/1470019129867239526/Your_paragraph_text_36.gif";
+const JJK_BOSS_VICTORY_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1470019159818637446/Your_paragraph_text_34.gif?ex=6989c587&is=69887407&hm=99b7163d50ac6f03bdfb132a1f38716d9f331a4e3fb395a033db8bbaaa4c5bed&=";
 
-const JJK_WIN =
-"https://media.discordapp.net/attachments/1405973335979851877/1470019159818637446/Your_paragraph_text_34.gif";
+const JJK_BOSS_DEFEAT_MEDIA =
+  "https://media.discordapp.net/attachments/1405973335979851877/1470019147839701177/Your_paragraph_text_35.gif?ex=6989c584&is=69887404&hm=366ad3f129479258f8d56f37c4ba5c3a7fd15e9767a79435f78e81e3f5a132d5&=";
 
-const JJK_LOSE =
-"https://media.discordapp.net/attachments/1405973335979851877/1470019147839701177/Your_paragraph_text_35.gif";
-
+/* ===================== JJK MOB (for now keep placeholder, we will change mob.js later) ===================== */
+const CURSED_SPIRIT_MEDIA = JJK_BOSS_SPAWN_MEDIA;
 
 module.exports = {
+  VASTO_SPAWN_MEDIA,
+  VASTO_R1,
+  VASTO_R2,
+  VASTO_R3,
+  VASTO_R4,
+  VASTO_R5,
+  VASTO_VICTORY_MEDIA,
+  VASTO_DEFEAT_MEDIA,
 
-  GRIMMJOW_SPAWN,
+  ULQ_SPAWN_MEDIA,
+  ULQ_R1,
+  ULQ_R2,
+  ULQ_R3,
+  ULQ_R4,
+  ULQ_R5,
+  ULQ_R6,
+  ULQ_VICTORY_MEDIA,
+  ULQ_DEFEAT_MEDIA,
+
+  GRIMMJOW_SPAWN_MEDIA,
   GRIMMJOW_R1,
   GRIMMJOW_R2,
-  GRIMMJOW_WIN,
-  GRIMMJOW_LOSE,
+  GRIMMJOW_VICTORY_MEDIA,
+  GRIMMJOW_DEFEAT_MEDIA,
 
-  JJK_SPAWN,
-  JJK_R1,
-  JJK_R2,
-  JJK_R3,
-  JJK_WIN,
-  JJK_LOSE,
+  HOLLOW_MEDIA,
+
+  JJK_BOSS_SPAWN_MEDIA,
+  JJK_BOSS_R1,
+  JJK_BOSS_R2,
+  JJK_BOSS_R3,
+  JJK_BOSS_VICTORY_MEDIA,
+  JJK_BOSS_DEFEAT_MEDIA,
+
+  CURSED_SPIRIT_MEDIA,
 };
