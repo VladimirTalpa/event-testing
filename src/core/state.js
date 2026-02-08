aa6e2b22b5e52130469ea3565cebfc0adb59bea4
VladimirTalpa <157706039+VladimirTalpa@users.noreply.github.com>
2026-02-08 14:49:35 +0200

    Update state.js

--- src/core/state.js
+++ src/core/state.js
@@ -1,4 +1,5 @@
-module.exports = {
-  bossByChannel: new Map(),
-  mobByChannel: new Map(),
-};
+// src/core/state.js
+const bossByChannel = new Map();
+const mobByChannel = new Map();
+
+module.exports = { bossByChannel, mobByChannel };
