b7443ad6b30df661b73376fce43aece55c4dfb17
VladimirTalpa <157706039+VladimirTalpa@users.noreply.github.com>
2026-02-13 21:27:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// src/index.js
 require("dotenv").config();
 
 const { Client, GatewayIntentBits, Partials, Events } = require("discord.js");
@@ -15,7 +16,6 @@ const client = new Client({
 client.once(Events.ClientReady, async () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
   await initRedis();
-  console.log("✅ Core ready");
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
