177389cff9277e6d0d38cddcc561aaaa1c3ec1be
VladimirTalpa <157706039+VladimirTalpa@users.noreply.github.com>
2026-02-13 14:53:20 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -15,6 +15,10 @@ const BOOSTER_ROLE_ID = "1267266564961341501";
 /* ===================== THEME / EMOJIS ===================== */
 const COLOR = 0x7b2cff;
 
+// Placeholder gif for ALL card visuals (until custom art exists)
+const CARD_GIF_PLACEHOLDER =
+  "https://cdn.discordapp.com/attachments/1468153576353431615/1471828355153268759/Your_paragraph_text.gif?ex=69905a79&is=698f08f9&hm=9d059092959a3446edcf38507f1a71b5577e85a97a8ee08292da323f238d513b&";
+
 // Event logos (your request)
 const E_BLEACH = "<:event:1470018874408964119>";
 const E_JJK = "<:event:1470018845245968434>";
