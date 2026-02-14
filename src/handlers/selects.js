c95074c0f83cb0f863c42a5ba4fdc626b7abe74c
VladimirTalpa <157706039+VladimirTalpa@users.noreply.github.com>
2026-02-14 22:42:18 +0200

    Update selects.js

--- src/handlers/selects.js
+++ src/handlers/selects.js
@@ -1,1 +1,5 @@
 
+module.exports = async function handleSelects(interaction) {
+  // пока не используем select-меню в Part 2
+  return interaction.reply({ content: "Select menu is not used yet.", ephemeral: true });
+};
