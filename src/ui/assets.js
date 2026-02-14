8ef3a1a79bb76b971a2f62e164b8f21eae52c4e2
VladimirTalpa <157706039+VladimirTalpa@users.noreply.github.com>
2026-02-14 21:55:57 +0200

    Delete src/ui/assets.js

--- src/ui/assets.js
+++
@@ -1,9 +0,0 @@
-// src/ui/assets.js
-
-// Твоя тест-гифка — ставим как "рамку/вид" карточек пока нет дизайна
-const CARD_FRAME_GIF =
-  "https://media.discordapp.net/attachments/1468153576353431615/1471828355153268759/Your_paragraph_text.gif?ex=69905a79&is=698f08f9&hm=9d059092959a3446edcf38507f1a71b5577e85a97a8ee08292da323f238d513b&=&width=388&height=582";
-
-module.exports = {
-  CARD_FRAME_GIF,
-};
